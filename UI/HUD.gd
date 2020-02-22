fbb2c7ff0df8a5170b8753ff710510fe3dc6750c
mariothedog <mariothedogmc@gmail.com>
2020-02-22 20:09:43 +0000

    Improved the inventory gui

--- UI/HUD.gd
+++ UI/HUD.gd
@@ -4,4 +4,4 @@ func _ready():
 	update_inventory()
 
 func update_inventory():
-	$Inventory/Iron/Label.text = "Iron Ore: %s" % global.inventory.get("Iron Ore", 0)
+	$Inventory/HBoxContainer/Label.text = "Iron Ore: %s" % global.inventory.get("Iron Ore", 0)
